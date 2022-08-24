9b38aa5166813ef65a2f5eaf563c8e435b3b19d5
nantipak tathong <auto0032@icloud.com>
2022-08-24 14:50:52 +0700

    Patient Detail and layout

---
+++ src/views/patient/PatientDetailView.vue
@@ -0,0 +1,16 @@
+\
+<template>
+  <p>{{ patient.name }}</p>
+  <p>{{ patient.surname }}</p>
+  <p>{{ patient.homeTown }}</p>
+  <p>{{ patient.age }}</p>
+  <p>{{ patient.vaccinateStatus }}</p>
+  <p>{{ patient.firstDose }}</p>
+  <p>{{ patient.secondDose }}</p>
+</template>
+
+<script>
+export default {
+  props: ["patient"],
+};
+</script>
